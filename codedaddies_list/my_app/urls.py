7cc3e8d5e3051d8d4e4bc921d78dc2a4e381c3b0
abdurehmansfa <abdurehmansafa13@gmail.com>
2019-10-11 10:10:00 -0900

    Add frontend requirements

--- codedaddies_list/my_app/urls.py
+++ codedaddies_list/my_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
+    path('new__search', views.new__search, name='new__search')
 
-}
+]
 
